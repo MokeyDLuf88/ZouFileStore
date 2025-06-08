d286a79eb20bfbee4193d11cd824a095b897f453
MokeyDLuf88 <pradeeshkuttayi@gmail.com>
2025-06-08 19:52:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -149,7 +149,7 @@ async def start_command(client: Client, message: Message):
     else:
         reply_markup = InlineKeyboardMarkup(
             [
-                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Anime_Zou)],
+                    [InlineKeyboardButton("• ᴍᴏʀᴇ ᴄʜᴀɴɴᴇʟs •", url="https://t.me/Anime_Zou")],
 
     [
                     InlineKeyboardButton("• ᴀʙᴏᴜᴛ", callback_data = "about"),
